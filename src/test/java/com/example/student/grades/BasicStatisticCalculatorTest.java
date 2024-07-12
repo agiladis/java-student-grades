77c55a8cad8496b6e6b554f426ae50dfe92b9180
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-07-12 22:51:16 +0700

    [ADD] test case positive for mode

--- src/test/java/com/example/student/grades/BasicStatisticCalculatorTest.java
+++ src/test/java/com/example/student/grades/BasicStatisticCalculatorTest.java
@@ -11,18 +11,27 @@ import java.util.List;
 class BasicStatisticCalculatorTest {
     private BasicStatisticCalculator basicStatisticCalculator;
     private List<Double> dummyData;
+    private List<Double> singelDummyData;
 
     @BeforeEach
     public void setup() {
         basicStatisticCalculator = new BasicStatisticCalculator();
         dummyData = Arrays.asList(8.0, 9.0, 8.0, 9.0, 3.0, 5.0, 10.0);
+        singelDummyData = Arrays.asList(2.0);
     }
 
     @Test
     @DisplayName("Positive test - Successfull calculate mode")
     void testModeSuccess() {
-        var mode = basicStatisticCalculator.mode(dummyData);
-        Assertions.assertEquals(8.0, mode);
+        Assertions.assertEquals(8.0, basicStatisticCalculator.mode(dummyData));
+        Assertions.assertEquals(2.0, basicStatisticCalculator.mode(singelDummyData));
+    }
+
+    @Test
+    @DisplayName("Positive test - Successfull calculate mode with unique data")
+    void testModeWithUniqueData() {
+        List<Double> uniqueData = Arrays.asList(4.0, 3.0, 1.0, 5.0, 6.0, 2.0);
+        Assertions.assertEquals(4.0, basicStatisticCalculator.mode(uniqueData));
     }
 
     @Test
