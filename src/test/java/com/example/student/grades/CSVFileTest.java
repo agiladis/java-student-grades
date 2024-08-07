6364b5e619abd1c75c8c59d864c356f0b06ee681
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-08-07 13:37:15 +0700

    [UPDATE] delete unused package

--- src/test/java/com/example/student/grades/CSVFileTest.java
+++ src/test/java/com/example/student/grades/CSVFileTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
